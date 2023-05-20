478d866fc03e3e10b503f21d74c0c2b3b84cc132
GaetanoComandatore <126972665+GaetanoComandatore@users.noreply.github.com>
2023-05-20 11:31:58 +0200

    Create buffer.java

---
+++ buffer.java
@@ -0,0 +1,13 @@
+
+/**
+ * Aggiungi qui una descrizione della classe Buffer
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public interface Buffer
+{
+    public void set( int value); // scrive nel buffer
+    public int get(); // legge dal buffer
+    public int capacity(); //rende la dimensione del buffer
+}
